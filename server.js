0ad64a5d1fb1e272d4c9fc43d6cbbe62bbc6d71a
mrinalpd <mrinalpdesai@gmail.com>
2019-01-09 14:44:01 +0530

    Changes to Projects page

--- server.js
+++ server.js
@@ -29,7 +29,10 @@ app.get('/',(req,res)=>
 
 app.get('/projects',(req,res)=>
 {
-    res.render('projects.hbs');
+    res.render('projects.hbs',
+    {
+        pageTitle: "Projects Page"
+    });
 });
 
 app.get('/about',(req,res)=>
